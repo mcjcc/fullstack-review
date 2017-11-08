1dde034576fee2bf1d207ecf93df9cd3d37222d6
Johnny Chen <mcjohnnychen@gmail.com>
2017-11-08 14:30:15 -0800

    Create database schema and pseudocode express get post functions

--- database/index.js
+++ database/index.js
@@ -2,7 +2,19 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+  id: Number,
+  name: String,
+  full_name: String,
+  owner_name: String,
+  owner_id: Number,
+  avatar_url: String,
+  html_url: String,
+  description: String,
+  forks_count: Number,
+  stargazers_count: Number,
+  watchers_count: Number,
+  open_issues_count: Number,
+  size: Number
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
--- server/index.js
+++ server/index.js
@@ -8,11 +8,18 @@ app.post('/repos', function (req, res) {
   // This route should take the github username provided
   // and get the repo information from the github API, then
   // save the repo information in the database
+
+  // call a helper function that takes in github username
+  // helper function makes an ajax request to github
+  // when request is complete and returns a list of repos from the username, call model.create on each repo in the db
 });
 
 app.get('/repos', function (req, res) {
   // TODO - your code here!
   // This route should send back the top 25 repos
+
+  // access repo model.get(<column name>,<top number of repos to retrieve>)
+  // respond with the results
 });
 
 let port = 1128;
@@ -20,4 +27,3 @@ let port = 1128;
 app.listen(port, function() {
   console.log(`listening on port ${port}`);
 });
-
